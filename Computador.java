ecb3e328b47af15ef0bc093d8c2d3fbecc3240ac
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-12-15 08:13:25 -0300

    Update Computador.java

--- Computador.java
+++ Computador.java
@@ -1,7 +1,26 @@
-public class Computador extends Equipamento{
-
-    private String nome;
-    private String curso;
-
-
+public class Computador extends Equipamento {
+    private int ram;
+    private String os;
+    public Computador(String id, String marca) {
+        super(id, "Computador", marca);
+    }
+    public int getRam() {
+        return ram;
+    }
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+    public void setOs(String os) {
+        this.os = os;
+    }
+    public String getOs() {
+        return os;
+    }
+    public String getInfoComp() {
+        return "Id: " + super.getId() +
+                "\nTipo: " + super.getTipo() +
+                "\nMarca: " + super.getMarca() +
+                "\nMemoria RAM: " + ram + "GB" +
+                "\nSistema Operacional: " + os;
+    }
 }
