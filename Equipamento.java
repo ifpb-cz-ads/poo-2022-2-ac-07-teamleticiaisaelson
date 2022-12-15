1c3a1584795e4905b7aea84de83265b1bea827e4
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-12-15 08:14:39 -0300

    Update Equipamento.java

--- Equipamento.java
+++ Equipamento.java
@@ -1,5 +1,36 @@
 
-public class Equipamento {
- private int codigo;
- private double preco;    
+ppublic class Equipamento {
+    private boolean ligado = false;
+    private String id;
+    private String tipo;
+    private String marca;
+
+    public Equipamento(String id, String tipo, String marca) {
+        this.id = id;
+        this.tipo = tipo;
+        this.marca = marca;
+    }
+    public void ligar() {
+        ligado = true;
+    }
+    public void desligar() {
+        ligado = false;
+    }
+    public boolean isLigado() {
+        return ligado;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public String getInfo() {
+        return "Id: " + id +
+                "\nTipo: " + tipo +
+                "\nMarca: " + marca;
+    }
 }
