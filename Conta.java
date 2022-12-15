3460a50ebd8516cc96ecc65352ff65477278892e
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-12-15 08:17:19 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,42 +1,32 @@
-##CTQuestão01##
-
-import java.util.ResourceBundle.Control;
-
-public class ContaEspecial extends Conta{
-    private double limite;
-
-    public double getLimite(){
-        return limite;
-    }
-    public void setLimite(double limite){
-        this.limite = limite; 
-    }
-}
-
-public class Conta extends Conta {
-    public void reajustar(double percentual){
-        double saldoAtual = this.getSaldo();
-        double reajuste = saldoAtual + percentual;
-        this.depositar(reajuste);
-    }
-}
-public ContaEspecial(int numero, String nome_titular, double limite){
-    super(numero,nome_titular);
-    this.limite=limite;
-}
-public Conta(int numero, String nome_titular){
-    super(numero,nome_titular);
-}
-public class ContaPoupanca extends Conta{
-    public void reajustar(double percentual){
-        saldo = saldo + saldo * percentual;
-    }
-}
-public boolean sacar(double valor){
-    if(valor <= this.limite + this.saldo){
-        this.saldo -= valor;
-        return true;
-    } else {
-        return false;
-    }
+public class Conta{
+   private int numero;
+   private String nome_titular;
+   private double saldo;
+   
+   public Conta (int numero, String nome_titular){
+       this.numero = numero;
+       this.nome_titular = nome_titular;
+   }
+   public void depositar(double valor){
+       this.saldo = this.getSaldo() + valor;
+   }
+   public boolean sacar(double valor){
+       if(this.getSaldo() >= valor){
+           this.saldo -= valor;
+           return true;
+       }
+       return false;
+   }
+   public double getSaldo(){
+       return saldo;
+   } 
+   public int getNumero(){
+       return numero;
+   }
+   public String getNome_titular(){
+       return nome_titular;
+   }
+   public void setNome_titular(String nome_titular){
+       this.nome_titular = nome_titular;
+   }
 }
